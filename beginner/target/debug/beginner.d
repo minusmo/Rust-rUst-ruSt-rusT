5284bb58b00d764edd7036e2f602a576c14eb8dd
hojooneum <bldolphin96@gmail.com>
2023-04-18 23:46:36 +0900

    first build and run with cargo!

---
+++ beginner/target/debug/beginner.d
@@ -0,0 +1,1 @@
+/Users/hojooneum/Rust-rUst-ruSt-rusT/beginner/target/debug/beginner: /Users/hojooneum/Rust-rUst-ruSt-rusT/beginner/src/main.rs
---
+++ beginner/target/debug/deps/beginner-81bcd3c3e7c022ba.d
@@ -0,0 +1,5 @@
+/Users/hojooneum/Rust-rUst-ruSt-rusT/beginner/target/debug/deps/beginner-81bcd3c3e7c022ba: src/main.rs
+
+/Users/hojooneum/Rust-rUst-ruSt-rusT/beginner/target/debug/deps/beginner-81bcd3c3e7c022ba.d: src/main.rs
+
+src/main.rs:
