546167b715120ded014a7ce93e45516f32931eb0
hojooneum <bldolphin96@gmail.com>
2023-04-19 23:23:59 +0900

    primitive practive!

--- beginner/src/main.rs
+++ beginner/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
+    let seven: i8 = 7;
+    let rate: f32 = 6.7;
+    let is_right: bool = false;
+    let mut eight: i8 = 8;
+    let random_number = 123214124;
+
     println!("{} is my name.", "Ho Joon");
 }
