bb59d0ac8735eafb5fcfd2d5662fd0a47e13168f
hojooneum <bldolphin96@gmail.com>
2023-04-19 23:51:26 +0900

    array and slice practice

--- beginner/src/main.rs
+++ beginner/src/main.rs
@@ -1,9 +1,6 @@
 fn main() {
-    let seven: i8 = 7;
-    let rate: f32 = 6.7;
-    let is_right: bool = false;
-    let mut eight: i8 = 8;
-    let random_number = 123214124;
-
-    println!("{} is my name.", "Ho Joon");
+    // This is an array
+    let array: [i32; 3] = [1,2,3];
+    // This is a slice
+    let slice: [i32; 500] = [1; 500];
 }
