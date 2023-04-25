c9b8aaaf029b4d3502185e1ce104414833d991f3
hojooneum <bldolphin96@gmail.com>
2023-04-25 23:36:02 +0900

    first for loop in rust!

--- beginner/src/main.rs
+++ beginner/src/main.rs
@@ -3,4 +3,8 @@ fn main() {
     let array: [i32; 3] = [1,2,3];
     // This is a slice
     let slice: [i32; 500] = [1; 500];
+
+    for i in 0..array.len() {
+        println!("{}",array[i]);
+    }
 }
