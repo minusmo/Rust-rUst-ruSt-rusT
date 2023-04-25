d1ce1cc6cf98b3c53cafb984c4a548ff4366272b
hojooneum <bldolphin96@gmail.com>
2023-04-25 23:52:50 +0900

    first if else statement!

--- beginner/src/main.rs
+++ beginner/src/main.rs
@@ -2,12 +2,14 @@ fn main() {
     // This is an array
     let array: [i32; 3] = [1,2,3];
     // This is a slice
-    let slice: [i32; 500] = [1; 500];
+    // let slice: [i32; 500] = [1; 500];
 
     for i in 0..array.len() {
-        match i {
-            1..=2 => println!("{}", i),
-            _ => println!("{}", "odd!"),
+        if array[i] % 2 == 0 {
+            println!("{}", i);
+        }
+        else {
+            println!("odd number")
         }
     }
 }
